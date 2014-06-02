2ef16197509c2a80f90709847a1bd4c07a965b4e
Alex Williams and Stephen Prater <dev+robovirtuoso+stephenprater@acumenbrands.com>
2014-06-02 17:06:29 -0500

    Add async functionality

--- lib/kraken-io.rb
+++ lib/kraken-io.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'httparty'
 require 'active_support/hash_with_indifferent_access'
 require 'active_support/core_ext/hash'
+require 'thread'
 
 require 'kraken-io/http_multi_part'
 require 'kraken-io/response'
@@ -20,27 +21,63 @@ module Kraken
       @api_secret = options.fetch(:api_secret)
     end
 
+    def async
+      @async = true
+      self
+    end
+
+    def sync
+      @async = false
+      self
+    end
+
     def url(url, params = {})
       params = normalized_params(params).merge!(auth_hash)
       params[:url] = url
-      res = self.class.post('/url', body: params.to_json)
-      res = Kraken::Response.new(res)
-      yield res if block_given? or return res
+      call_kraken do
+        res = self.class.post('/url', body: params.to_json)
+        res = Kraken::Response.new(res)
+        yield res if block_given? or return res
+      end
+    end
+
+    def callback_url(url)
+      @callback_url = url
+      self
     end
 
     def upload(file_name, params = {})
       params = normalized_params(params).merge!(auth_hash)
-      res = self.class.multipart_post('/upload', file: file_name, body: params.to_json)
-      res = Kraken::Response.new(res)
-      yield res if block_given? or return res
+      call_kraken do
+        res = self.class.multipart_post('/upload', file: file_name, body: params.to_json)
+        res = Kraken::Response.new(res)
+        yield res if block_given? or return res
+      end
     end
 
     private
+    def call_kraken(&block)
+      if @async
+        call_async(&block)
+      else
+        yield
+      end
+    end
+
+    def call_async(&block)
+      Thread.abort_on_exception = false
+      Thread.new do |t|
+        block.call
+      end
+      nil
+    end
 
     def normalized_params(params)
       params = params.with_indifferent_access
 
-      unless params.keys.include?(:callback)
+      if params.keys.include?(:callback_url) || @callback_url
+        params[:callback_url] = @callback_url
+      else
         params[:wait] = true
       end
 
